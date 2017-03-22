1c213a7f4f0517b68f7f49292e7999fa62f61a55
Qiao <qiaoy@cmu.edu>
2017-03-22 19:01:33 -0400

    upload pie char html js and css

---
+++ js/index.js
@@ -0,0 +1,1010 @@
+/**
+ * Define data for each year
+ */
+var chartData = {
+  "1970": [{
+    "sector": "Meat",
+    "size": 506
+  }, {
+    "sector": "Dairy",
+    "size": 234
+  }, {
+    "sector": "Fruit",
+    "size": 67
+  }, {
+    "sector": "Vegetables",
+    "size": 129
+  }, {
+    "sector": "Grains",
+    "size": 409
+  }, {
+    "sector": "Fats",
+    "size": 346
+  }, {
+    "sector": "Sugars",
+    "size": 333
+  }],
+
+  "1971": [{
+    "sector": "Meat",
+    "size": 514
+  }, {
+    "sector": "Dairy",
+    "size": 236
+  }, {
+    "sector": "Fruit",
+    "size": 69
+  }, {
+    "sector": "Vegetables",
+    "size": 129
+  }, {
+    "sector": "Grains",
+    "size": 403
+  }, {
+    "sector": "Fats",
+    "size": 342
+  }, {
+    "sector": "Sugars",
+    "size": 335
+  }],
+
+  "1972": [{
+    "sector": "Meat",
+    "size": 513
+  }, {
+    "sector": "Dairy",
+    "size": 234
+  }, {
+    "sector": "Fruit",
+    "size": 65
+  }, {
+    "sector": "Vegetables",
+    "size": 128
+  }, {
+    "sector": "Grains",
+    "size": 398
+  }, {
+    "sector": "Fats",
+    "size": 353
+  }, {
+    "sector": "Sugars",
+    "size": 339
+  }],
+
+  "1973": [{
+    "sector": "Meat",
+    "size": 485
+  }, {
+    "sector": "Dairy",
+    "size": 233
+  }, {
+    "sector": "Fruit",
+    "size": 68
+  }, {
+    "sector": "Vegetables",
+    "size": 128
+  }, {
+    "sector": "Grains",
+    "size": 410
+  }, {
+    "sector": "Fats",
+    "size": 357
+  }, {
+    "sector": "Sugars",
+    "size": 340
+  }],
+
+  "1974": [{
+    "sector": "Meat",
+    "size": 501
+  }, {
+    "sector": "Dairy",
+    "size": 226
+  }, {
+    "sector": "Fruit",
+    "size": 69
+  }, {
+    "sector": "Vegetables",
+    "size": 123
+  }, {
+    "sector": "Grains",
+    "size": 406
+  }, {
+    "sector": "Fats",
+    "size": 349
+  }, {
+    "sector": "Sugars",
+    "size": 329
+  }],
+
+  "1975": [{
+    "sector": "Meat",
+    "size": 492
+  }, {
+    "sector": "Dairy",
+    "size": 224
+  }, {
+    "sector": "Fruit",
+    "size": 72
+  }, {
+    "sector": "Vegetables",
+    "size": 127
+  }, {
+    "sector": "Grains",
+    "size": 415
+  }, {
+    "sector": "Fats",
+    "size": 351
+  }, {
+    "sector": "Sugars",
+    "size": 318
+  }],
+
+  "1976": [{
+    "sector": "Meat",
+    "size": 515
+  }, {
+    "sector": "Dairy",
+    "size": 227
+  }, {
+    "sector": "Fruit",
+    "size": 73
+  }, {
+    "sector": "Vegetables",
+    "size": 128
+  }, {
+    "sector": "Grains",
+    "size": 429
+  }, {
+    "sector": "Fats",
+    "size": 367
+  }, {
+    "sector": "Sugars",
+    "size": 335
+  }],
+
+  "1977": [{
+    "sector": "Meat",
+    "size": 510
+  }, {
+    "sector": "Dairy",
+    "size": 225
+  }, {
+    "sector": "Fruit",
+    "size": 74
+  }, {
+    "sector": "Vegetables",
+    "size": 128
+  }, {
+    "sector": "Grains",
+    "size": 417
+  }, {
+    "sector": "Fats",
+    "size": 353
+  }, {
+    "sector": "Sugars",
+    "size": 342
+  }],
+
+  "1978": [{
+    "sector": "Meat",
+    "size": 505
+  }, {
+    "sector": "Dairy",
+    "size": 224
+  }, {
+    "sector": "Fruit",
+    "size": 73
+  }, {
+    "sector": "Vegetables",
+    "size": 123
+  }, {
+    "sector": "Grains",
+    "size": 426
+  }, {
+    "sector": "Fats",
+    "size": 364
+  }, {
+    "sector": "Sugars",
+    "size": 339
+  }],
+
+  "1979": [{
+    "sector": "Meat",
+    "size": 500
+  }, {
+    "sector": "Dairy",
+    "size": 223
+  }, {
+    "sector": "Fruit",
+    "size": 73
+  }, {
+    "sector": "Vegetables",
+    "size": 126
+  }, {
+    "sector": "Grains",
+    "size": 431
+  }, {
+    "sector": "Fats",
+    "size": 372
+  }, {
+    "sector": "Sugars",
+    "size": 342
+  }],
+
+  "1980": [{
+    "sector": "Meat",
+    "size": 494
+  }, {
+    "sector": "Dairy",
+    "size": 219
+  }, {
+    "sector": "Fruit",
+    "size": 77
+  }, {
+    "sector": "Vegetables",
+    "size": 123
+  }, {
+    "sector": "Grains",
+    "size": 437
+  }, {
+    "sector": "Fats",
+    "size": 372
+  }, {
+    "sector": "Sugars",
+    "size": 335
+  }],
+
+  "1981": [{
+    "sector": "Meat",
+    "size": 499
+  }, {
+    "sector": "Dairy",
+    "size": 217
+  }, {
+    "sector": "Fruit",
+    "size": 74
+  }, {
+    "sector": "Vegetables",
+    "size": 122
+  }, {
+    "sector": "Grains",
+    "size": 437
+  }, {
+    "sector": "Fats",
+    "size": 370
+  }, {
+    "sector": "Sugars",
+    "size": 334
+  }],
+
+  "1982": [{
+    "sector": "Meat",
+    "size": 493
+  }, {
+    "sector": "Dairy",
+    "size": 221
+  }, {
+    "sector": "Fruit",
+    "size": 77
+  }, {
+    "sector": "Vegetables",
+    "size": 125
+  }, {
+    "sector": "Grains",
+    "size": 436
+  }, {
+    "sector": "Fats",
+    "size": 372
+  }, {
+    "sector": "Sugars",
+    "size": 328
+  }],
+
+  "1983": [{
+    "sector": "Meat",
+    "size": 503
+  }, {
+    "sector": "Dairy",
+    "size": 224
+  }, {
+    "sector": "Fruit",
+    "size": 83
+  }, {
+    "sector": "Vegetables",
+    "size": 127
+  }, {
+    "sector": "Grains",
+    "size": 437
+  }, {
+    "sector": "Fats",
+    "size": 387
+  }, {
+    "sector": "Sugars",
+    "size": 333
+  }],
+
+  "1984": [{
+    "sector": "Meat",
+    "size": 508
+  }, {
+    "sector": "Dairy",
+    "size": 228
+  }, {
+    "sector": "Fruit",
+    "size": 80
+  }, {
+    "sector": "Vegetables",
+    "size": 127
+  }, {
+    "sector": "Grains",
+    "size": 447
+  }, {
+    "sector": "Fats",
+    "size": 402
+  }, {
+    "sector": "Sugars",
+    "size": 340
+  }],
+
+  "1985": [{
+    "sector": "Meat",
+    "size": 516
+  }, {
+    "sector": "Dairy",
+    "size": 231
+  }, {
+    "sector": "Fruit",
+    "size": 81
+  }, {
+    "sector": "Vegetables",
+    "size": 130
+  }, {
+    "sector": "Grains",
+    "size": 475
+  }, {
+    "sector": "Fats",
+    "size": 424
+  }, {
+    "sector": "Sugars",
+    "size": 352
+  }],
+
+  "1986": [{
+    "sector": "Meat",
+    "size": 513
+  }, {
+    "sector": "Dairy",
+    "size": 232
+  }, {
+    "sector": "Fruit",
+    "size": 84
+  }, {
+    "sector": "Vegetables",
+    "size": 132
+  }, {
+    "sector": "Grains",
+    "size": 489
+  }, {
+    "sector": "Fats",
+    "size": 431
+  }, {
+    "sector": "Sugars",
+    "size": 347
+  }],
+
+  "1987": [{
+    "sector": "Meat",
+    "size": 509
+  }, {
+    "sector": "Dairy",
+    "size": 234
+  }, {
+    "sector": "Fruit",
+    "size": 88
+  }, {
+    "sector": "Vegetables",
+    "size": 130
+  }, {
+    "sector": "Grains",
+    "size": 513
+  }, {
+    "sector": "Fats",
+    "size": 427
+  }, {
+    "sector": "Sugars",
+    "size": 359
+  }],
+
+  "1988": [{
+    "sector": "Meat",
+    "size": 516
+  }, {
+    "sector": "Dairy",
+    "size": 229
+  }, {
+    "sector": "Fruit",
+    "size": 86
+  }, {
+    "sector": "Vegetables",
+    "size": 131
+  }, {
+    "sector": "Grains",
+    "size": 525
+  }, {
+    "sector": "Fats",
+    "size": 433
+  }, {
+    "sector": "Sugars",
+    "size": 363
+  }],
+
+  "1989": [{
+    "sector": "Meat",
+    "size": 507
+  }, {
+    "sector": "Dairy",
+    "size": 222
+  }, {
+    "sector": "Fruit",
+    "size": 88
+  }, {
+    "sector": "Vegetables",
+    "size": 131
+  }, {
+    "sector": "Grains",
+    "size": 520
+  }, {
+    "sector": "Fats",
+    "size": 413
+  }, {
+    "sector": "Sugars",
+    "size": 358
+  }],
+
+  "1990": [{
+    "sector": "Meat",
+    "size": 499
+  }, {
+    "sector": "Dairy",
+    "size": 223
+  }, {
+    "sector": "Fruit",
+    "size": 81
+  }, {
+    "sector": "Vegetables",
+    "size": 132
+  }, {
+    "sector": "Grains",
+    "size": 540
+  }, {
+    "sector": "Fats",
+    "size": 411
+  }, {
+    "sector": "Sugars",
+    "size": 369
+  }],
+
+  "1991": [{
+    "sector": "Meat",
+    "size": 502
+  }, {
+    "sector": "Dairy",
+    "size": 222
+  }, {
+    "sector": "Fruit",
+    "size": 83
+  }, {
+    "sector": "Vegetables",
+    "size": 139
+  }, {
+    "sector": "Grains",
+    "size": 542
+  }, {
+    "sector": "Fats",
+    "size": 419
+  }, {
+    "sector": "Sugars",
+    "size": 370
+  }],
+
+  "1992": [{
+    "sector": "Meat",
+    "size": 510
+  }, {
+    "sector": "Dairy",
+    "size": 223
+  }, {
+    "sector": "Fruit",
+    "size": 85
+  }, {
+    "sector": "Vegetables",
+    "size": 138
+  }, {
+    "sector": "Grains",
+    "size": 550
+  }, {
+    "sector": "Fats",
+    "size": 433
+  }, {
+    "sector": "Sugars",
+    "size": 379
+  }],
+
+  "1993": [{
+    "sector": "Meat",
+    "size": 508
+  }, {
+    "sector": "Dairy",
+    "size": 218
+  }, {
+    "sector": "Fruit",
+    "size": 89
+  }, {
+    "sector": "Vegetables",
+    "size": 141
+  }, {
+    "sector": "Grains",
+    "size": 566
+  }, {
+    "sector": "Fats",
+    "size": 453
+  }, {
+    "sector": "Sugars",
+    "size": 388
+  }],
+
+  "1994": [{
+    "sector": "Meat",
+    "size": 512
+  }, {
+    "sector": "Dairy",
+    "size": 221
+  }, {
+    "sector": "Fruit",
+    "size": 88
+  }, {
+    "sector": "Vegetables",
+    "size": 141
+  }, {
+    "sector": "Grains",
+    "size": 571
+  }, {
+    "sector": "Fats",
+    "size": 443
+  }, {
+    "sector": "Sugars",
+    "size": 396
+  }],
+
+  "1995": [{
+    "sector": "Meat",
+    "size": 510
+  }, {
+    "sector": "Dairy",
+    "size": 219
+  }, {
+    "sector": "Fruit",
+    "size": 85
+  }, {
+    "sector": "Vegetables",
+    "size": 140
+  }, {
+    "sector": "Grains",
+    "size": 566
+  }, {
+    "sector": "Fats",
+    "size": 434
+  }, {
+    "sector": "Sugars",
+    "size": 402
+  }],
+
+  "1996": [{
+    "sector": "Meat",
+    "size": 508
+  }, {
+    "sector": "Dairy",
+    "size": 219
+  }, {
+    "sector": "Fruit",
+    "size": 88
+  }, {
+    "sector": "Vegetables",
+    "size": 145
+  }, {
+    "sector": "Grains",
+    "size": 588
+  }, {
+    "sector": "Fats",
+    "size": 424
+  }, {
+    "sector": "Sugars",
+    "size": 408
+  }],
+
+  "1997": [{
+    "sector": "Meat",
+    "size": 506
+  }, {
+    "sector": "Dairy",
+    "size": 218
+  }, {
+    "sector": "Fruit",
+    "size": 88
+  }, {
+    "sector": "Vegetables",
+    "size": 140
+  }, {
+    "sector": "Grains",
+    "size": 588
+  }, {
+    "sector": "Fats",
+    "size": 427
+  }, {
+    "sector": "Sugars",
+    "size": 417
+  }],
+
+  "1998": [{
+    "sector": "Meat",
+    "size": 520
+  }, {
+    "sector": "Dairy",
+    "size": 217
+  }, {
+    "sector": "Fruit",
+    "size": 90
+  }, {
+    "sector": "Vegetables",
+    "size": 138
+  }, {
+    "sector": "Grains",
+    "size": 581
+  }, {
+    "sector": "Fats",
+    "size": 425
+  }, {
+    "sector": "Sugars",
+    "size": 421
+  }],
+
+  "1999": [{
+    "sector": "Meat",
+    "size": 536
+  }, {
+    "sector": "Dairy",
+    "size": 221
+  }, {
+    "sector": "Fruit",
+    "size": 91
+  }, {
+    "sector": "Vegetables",
+    "size": 140
+  }, {
+    "sector": "Grains",
+    "size": 586
+  }, {
+    "sector": "Fats",
+    "size": 444
+  }, {
+    "sector": "Sugars",
+    "size": 427
+  }],
+
+  "2000": [{
+    "sector": "Meat",
+    "size": 533
+  }, {
+    "sector": "Dairy",
+    "size": 221
+  }, {
+    "sector": "Fruit",
+    "size": 88
+  }, {
+    "sector": "Vegetables",
+    "size": 141
+  }, {
+    "sector": "Grains",
+    "size": 596
+  }, {
+    "sector": "Fats",
+    "size": 545
+  }, {
+    "sector": "Sugars",
+    "size": 420
+  }],
+
+  "2001": [{
+    "sector": "Meat",
+    "size": 529
+  }, {
+    "sector": "Dairy",
+    "size": 221
+  }, {
+    "sector": "Fruit",
+    "size": 87
+  }, {
+    "sector": "Vegetables",
+    "size": 141
+  }, {
+    "sector": "Grains",
+    "size": 583
+  }, {
+    "sector": "Fats",
+    "size": 557
+  }, {
+    "sector": "Sugars",
+    "size": 414
+  }],
+
+  "2002": [{
+    "sector": "Meat",
+    "size": 546
+  }, {
+    "sector": "Dairy",
+    "size": 223
+  }, {
+    "sector": "Fruit",
+    "size": 86
+  }, {
+    "sector": "Vegetables",
+    "size": 136
+  }, {
+    "sector": "Grains",
+    "size": 572
+  }, {
+    "sector": "Fats",
+    "size": 588
+  }, {
+    "sector": "Sugars",
+    "size": 412
+  }],
+
+  "2003": [{
+    "sector": "Meat",
+    "size": 545
+  }, {
+    "sector": "Dairy",
+    "size": 221
+  }, {
+    "sector": "Fruit",
+    "size": 87
+  }, {
+    "sector": "Vegetables",
+    "size": 141
+  }, {
+    "sector": "Grains",
+    "size": 575
+  }, {
+    "sector": "Fats",
+    "size": 587
+  }, {
+    "sector": "Sugars",
+    "size": 400
+  }],
+
+  "2004": [{
+    "sector": "Meat",
+    "size": 553
+  }, {
+    "sector": "Dairy",
+    "size": 223
+  }, {
+    "sector": "Fruit",
+    "size": 88
+  }, {
+    "sector": "Vegetables",
+    "size": 138
+  }, {
+    "sector": "Grains",
+    "size": 570
+  }, {
+    "sector": "Fats",
+    "size": 586
+  }, {
+    "sector": "Sugars",
+    "size": 400
+  }],
+
+  "2005": [{
+    "sector": "Meat",
+    "size": 545
+  }, {
+    "sector": "Dairy",
+    "size": 223
+  }, {
+    "sector": "Fruit",
+    "size": 86
+  }, {
+    "sector": "Vegetables",
+    "size": 132
+  }, {
+    "sector": "Grains",
+    "size": 571
+  }, {
+    "sector": "Fats",
+    "size": 586
+  }, {
+    "sector": "Sugars",
+    "size": 401
+  }],
+
+  "2006": [{
+    "sector": "Meat",
+    "size": 549
+  }, {
+    "sector": "Dairy",
+    "size": 222
+  }, {
+    "sector": "Fruit",
+    "size": 85
+  }, {
+    "sector": "Vegetables",
+    "size": 134
+  }, {
+    "sector": "Grains",
+    "size": 580
+  }, {
+    "sector": "Fats",
+    "size": 585
+  }, {
+    "sector": "Sugars",
+    "size": 393
+  }],
+
+  "2007": [{
+    "sector": "Meat",
+    "size": 547
+  }, {
+    "sector": "Dairy",
+    "size": 222
+  }, {
+    "sector": "Fruit",
+    "size": 85
+  }, {
+    "sector": "Vegetables",
+    "size": 133
+  }, {
+    "sector": "Grains",
+    "size": 589
+  }, {
+    "sector": "Fats",
+    "size": 584
+  }, {
+    "sector": "Sugars",
+    "size": 382
+  }],
+
+  "2008": [{
+    "sector": "Meat",
+    "size": 533
+  }, {
+    "sector": "Dairy",
+    "size": 219
+  }, {
+    "sector": "Fruit",
+    "size": 82
+  }, {
+    "sector": "Vegetables",
+    "size": 126
+  }, {
+    "sector": "Grains",
+    "size": 586
+  }, {
+    "sector": "Fats",
+    "size": 594
+  }, {
+    "sector": "Sugars",
+    "size": 383
+  }],
+
+  "2009": [{
+    "sector": "Meat",
+    "size": 527
+  }, {
+    "sector": "Dairy",
+    "size": 222
+  }, {
+    "sector": "Fruit",
+    "size": 80
+  }, {
+    "sector": "Vegetables",
+    "size": 122
+  }, {
+    "sector": "Grains",
+    "size": 580
+  }, {
+    "sector": "Fats",
+    "size": 551
+  }, {
+    "sector": "Sugars",
+    "size": 369
+  }],
+
+  "2010": [{
+    "sector": "Meat",
+    "size": 525
+  }, {
+    "sector": "Dairy",
+    "size": 221
+  }, {
+    "sector": "Fruit",
+    "size": 81
+  }, {
+    "sector": "Vegetables",
+    "size": 126
+  }, {
+    "sector": "Grains",
+    "size": 581
+  }, {
+    "sector": "Fats",
+    "size": 575
+  }, {
+    "sector": "Sugars",
+    "size": 373
+  }],
+};
+
+/**
+ * Create the chart
+ */
+var currentYear = 1970;
+var chart = AmCharts.makeChart("chartdiv", {
+  "type": "pie",
+  "theme": "light",
+  "dataProvider": [],
+  "valueField": "size",
+  "titleField": "sector",
+  "startDuration": 0,
+  "innerRadius": 80,
+  "pullOutRadius": 20,
+  "marginTop": 30,
+  "titles": [{
+    "text": "Average American Diet in Calories"
+  }],
+  "allLabels": [{
+    "y": "54%",
+    "align": "center",
+    "size": 25,
+    "bold": true,
+    "text": "1970",
+    "color": "#555"
+  }, {
+    "y": "49%",
+    "align": "center",
+    "size": 15,
+    "text": "Year",
+    "color": "#555"
+  }],
+  "listeners": [{
+    "event": "init",
+    "method": function(e) {
+      var chart = e.chart;
+
+      function getCurrentData() {
+        var data = chartData[currentYear];
+        currentYear++;
+        if (currentYear > 2010)
+          currentYear = 1970;
+        return data;
+      }
+
+      function loop() {
+        chart.allLabels[0].text = currentYear;
+        var data = getCurrentData();
+        chart.animateData(data, {
+          duration: 1000,
+          complete: function() {
+            setTimeout(loop, 800);
+          }
+        });
+      }
+
+      loop();
+    }
+  }],
+  "export": {
+    "enabled": true
+  }
+});
